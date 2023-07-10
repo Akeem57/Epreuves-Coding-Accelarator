9431cfc6074cea59f1212dc6a462d7b9bb48d1e0
Akeem57 <lyrlandais@hotmail.com>
2023-07-10 21:35:20 +0200

    fin exercice terre00

--- terre00.rb
+++ terre00.rb
@@ -1,3 +1,8 @@
-puts 'Hello world'
-puts "Salut la commu :p"
-puts "TEST3"
+
+alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+i=0
+while alphabet[i]
+    print alphabet[i]
+    i=i+1
+end 
+puts ""
