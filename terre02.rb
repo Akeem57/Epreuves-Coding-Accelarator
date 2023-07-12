f3ebc9111e05f728dbfac0b933df8550d2401e37
Akeem57 <lyrlandais@hotmail.com>
2023-07-12 18:53:29 +0200

    fin terre02

--- terre02.rb
+++ terre02.rb
@@ -1,7 +1,7 @@
 #Créez un programme qui affiche les arguments qu’il reçoit ligne par ligne, 
 #peu importe le nombre d’arguments.
 arguments = ARGV #ARGV un tableau contenant les arguments passés à un script Ruby via la ligne de commande (shell)
-tailleArgv = arguments.size # size renvoi le nombre d'éléments dans une collection (tableau, hash, ..)
+tailleArgv = arguments.size #size renvoi le nombre d'éléments dans une collection (tableau, hash, ..)
 i = 0
 while i < tailleArgv
     puts arguments[i]
