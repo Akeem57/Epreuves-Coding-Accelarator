ea87de9215e7531843c0298588f1409caf02e205
Akeem57 <lyrlandais@hotmail.com>
2023-07-12 20:35:00 +0200

    Sur la voie des variable globales :p

--- terre03.rb
+++ terre03.rb
@@ -2,9 +2,9 @@
 # argument en lettres minuscules suivi d’un retour à la ligne.
 # utiliser une boucle !
 
-alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r"
-            ,"s","t","u","v","w","x","y","z"]
-arguments = ARGV
+alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+
+puts $* # Variable globale réservée 
 
 
 
