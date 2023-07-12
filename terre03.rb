4388fc326fc8bf8147c71285fd40a41956e5e72b
Akeem57 <lyrlandais@hotmail.com>
2023-07-12 19:56:08 +0200

    en cours terre03

--- terre03.rb
+++ terre03.rb
@@ -1,4 +1,10 @@
 # Créez un programme qui affiche l’alphabet à partir de la lettre donnée en 
-#argument en lettres minuscules suivi d’un retour à la ligne.
+# argument en lettres minuscules suivi d’un retour à la ligne.
 # utiliser une boucle !
+
+alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r"
+            ,"s","t","u","v","w","x","y","z"]
+arguments = ARGV
+
+
 
