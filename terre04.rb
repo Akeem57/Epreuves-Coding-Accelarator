0c6b269c3150a5d7520cc484ccdc0f5a505e5a94
Akeem57 <lyrlandais@hotmail.com>
2023-07-13 21:36:46 +0200

    en cours

--- terre04.rb
+++ terre04.rb
@@ -5,3 +5,12 @@ Gérez aussi les entiers négatifs
 Gérez les exceptions !   
 =end
 
+argument = ARGV[0]
+if argument % 2 == 0
+    puts "pair"
+elsif argument % 2 > 0
+    puts "impair"
+elsif argument.match?(/\A-?\d+(\.\d+)?\z/) 
+    puts "Arrête de déconner et donne moi un nombre"
+end
+puts""
