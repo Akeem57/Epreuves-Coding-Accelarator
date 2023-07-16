5535f999d91709529d23ac0e335df745c8f32962
Akeem57 <lyrlandais@hotmail.com>
2023-07-16 08:35:04 +0200

    fin terre07

--- terre07.rb
+++ terre07.rb
@@ -3,3 +3,19 @@ Créez un programme qui affiche le nombre de caractères d’une chaîne
 de caractères passée en argument.
 =end
 
+argument = ARGV
+longueurArg = argument.length
+noNum = argument[0].to_i.to_s
+
+
+begin
+    if longueurArg == 1 && noNum != argument[0] && argument != 0
+        longueurIndex = argument[0].length
+        puts longueurIndex     
+    else
+        puts "error"
+    end    
+rescue
+    puts "error2"
+end
+
