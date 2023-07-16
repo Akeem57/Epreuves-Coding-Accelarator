0c701dd7e924f1e4ec4aeaae9d317b59be3c55c6
Akeem57 <lyrlandais@hotmail.com>
2023-07-16 19:42:31 +0200

    fin terre13

--- terre13.rb
+++ terre13.rb
@@ -2,3 +2,20 @@
 Créez un programme qui prend en paramètre trois entiers et affiche la valeur du milieu.
 30 15 26 => 26
 =end
+
+argument = ARGV
+arg1 = argument[0].to_i
+arg2 = argument[1].to_i
+arg3 = argument[2].to_i
+
+    if (arg1 < arg2 && arg2 < arg3) || (arg3 < arg2 && arg2 < arg1) 
+        puts "#{arg2}"
+    elsif (arg2 < arg1 && arg1 < arg3) || (arg3 < arg1 && arg1 < arg2)
+        puts "#{arg1}"
+    elsif (arg1 < arg3 && arg3 < arg2) || (arg2 < arg3 && arg3 < arg1)
+        puts "#{arg3}"
+    elsif (arg1 = arg2 = arg3)
+        puts "error"
+    else
+    end
+
