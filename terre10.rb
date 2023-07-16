fcf3e55a7dcfdb3dc5a85742930245c0ed2e219e
Akeem57 <lyrlandais@hotmail.com>
2023-07-16 13:42:37 +0200

    debut

--- terre10.rb
+++ terre10.rb
@@ -5,13 +5,28 @@ Créez un programme qui affiche si un nombre est premier ou pas.
 argument = ARGV
 argument1 = ARGV[0].to_i
 longArg = argument.length
-nombres = [1,2,3,4,5,6,7,8,9]
+d = 2 # dénominateur
+
 begin
     if argument1.to_s == argument[0] && longArg < 2 && argument1 != 1 && argument1 != 0
-        if argument1 % 2 == 0
-            puts argument1
+        while d <= argument1 
+            modulo = argument1 % d
+                if modulo == 0 && d != argument1
+                    puts "Non, #{argument1.to_i} n'est pas un nombre premier"
+                    
+                    break
+                elsif d == agrument1
+                    puts "Oui, #{argument1.to_i} est un nombre premier"
+                    break
+                else
+                    puts modulo
+                    d = d+1 
+                    next
+                end
+        end
     else
         puts "Veuillez entrer un nombre, sauf 1 et 0"    
     end
 rescue
 end
+
