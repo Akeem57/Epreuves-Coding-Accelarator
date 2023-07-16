574e37cc4ca4f8b2bfb4c47c2fb2279c4c142c74
Akeem57 <lyrlandais@hotmail.com>
2023-07-16 09:32:41 +0200

    fin terre09

--- terre09.rb
+++ terre09.rb
@@ -3,3 +3,19 @@ Créez un programme qui affiche la racine carrée d’un entier positif.
 !! Gérer les erreurs d'arguments
 =end
 
+argument = ARGV
+argument1 = ARGV[0].to_i
+longArg = argument.length
+noChar = argument1.to_s
+
+begin
+    if longArg == 1 && noChar == ARGV[0] && argument1 > 0 
+       resultat = argument1 ** 0.5 
+       puts resultat.to_i
+
+    else 
+        puts "Veuillez entrer un entier positif"
+    end
+rescue
+
+end
