e9b3a9da8e934bac0a5b015af6dfea4e5692f5c4
Akeem57 <lyrlandais@hotmail.com>
2023-07-17 21:49:06 +0200

    fin terre13

--- terre13.rb
+++ terre13.rb
@@ -4,18 +4,24 @@ Créez un programme qui prend en paramètre trois entiers et affiche la valeur d
 =end
 
 argument = ARGV
-arg1 = argument[0].to_i
-arg2 = argument[1].to_i
-arg3 = argument[2].to_i
-
-    if (arg1 < arg2 && arg2 < arg3) || (arg3 < arg2 && arg2 < arg1) 
-        puts "#{arg2}"
-    elsif (arg2 < arg1 && arg1 < arg3) || (arg3 < arg1 && arg1 < arg2)
-        puts "#{arg1}"
-    elsif (arg1 < arg3 && arg3 < arg2) || (arg2 < arg3 && arg3 < arg1)
-        puts "#{arg3}"
-    elsif (arg1 = arg2 = arg3)
+longArg = argument.length
+arg1 = argument[0]
+arg2 = argument[1]
+arg3 = argument[2]
+argInt1 = argument[0].to_i
+argInt2 = argument[1].to_i
+argInt3 = argument[2].to_i
+if longArg == 3 && argInt1.to_s == arg1 && argInt2.to_s == arg2 && argInt3.to_s == arg3
+    if (argInt1 < argInt2 && argInt2 < argInt3) || (argInt3 < argInt2 && argInt2 < argInt1) 
+        puts "#{argInt2}"
+    elsif (argInt2 < argInt1 && argInt1 < argInt3) || (argInt3 < argInt1 && argInt1 < argInt2)
+        puts "#{argInt1}"
+    elsif (argInt1 < argInt3 && argInt3 < argInt2) || (argInt2 < argInt3 && argInt3 < argInt1)
+        puts "#{argInt3}"
+    elsif (argInt1 = argInt2 = argInt3)
         puts "error"
     else
     end
-
+else 
+    puts "Veuillez entrez 3 nombres entier"
+end
