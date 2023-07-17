6b607e30e8543282129a8d78d43e15b828d6f670
Akeem57 <lyrlandais@hotmail.com>
2023-07-17 21:56:46 +0200

    debut terre12

--- terre12.rb
+++ terre12.rb
@@ -1,5 +1,5 @@
 =begin
 Créez un programme qui transforme une heure affichée en format 12h 
 en une heure affichée au format 24h.
-!!Attention midi et minuit
+!!Attention midi et minuit argument 11:40PM
 =end
