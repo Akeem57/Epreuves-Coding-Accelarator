c84c0aa89967163881518ac87fa154202ee967bc
Akeem57 <lyrlandais@hotmail.com>
2023-07-19 20:08:41 +0200

    fin terre12

--- terre12.rb
+++ terre12.rb
@@ -6,12 +6,17 @@ en une heure affichée au format 24h.
 
 argument = ARGV
 longArg = argument.length
-argWash = argument[0].gsub(/[:]/, "")
-heures = argWash[0..1]
-minutes = argWash[2..3]
-ampm = argWash[4..5]
-argError = argWash.length
-if longArg == 1 && heures.to_i.to_s == heures && minutes.to_i.to_s == minutes && (ampm == "AM" || ampm == "PM") && argError == 6
+heures = argument[0].split(":")[0]
+minutes = argument[0].split(":")[1][0,2]
+ampm = argument[0].split(":")[1][2,4]
+heuresOnlyNumber = heures.match?(/[0-9]/)
+minutesOnlyNumber = minutes.match?(/[0-9]/)
+heuresLong = heures.length
+minutesLong = heures.length
+ampmLong = ampm.length
+if longArg == 1 && heuresOnlyNumber == true && minutesOnlyNumber == true \
+    && (ampm == "AM" || ampm == "PM") && heuresLong <= 2 && heuresLong >=1 \
+    && minutesLong <= 2 && minutesLong >= 1 && ampmLong == 2 && heures.to_i > 0
     if heures.to_i < 12 && ampm == "AM"
         puts "#{heures}:#{minutes}"
     elsif heures.to_i < 12 && ampm == "PM"
@@ -21,8 +26,9 @@ if longArg == 1 && heures.to_i.to_s == heures && minutes.to_i.to_s == minutes &&
     elsif heures.to_i == 12 && ampm == "PM"
         puts "12:#{minutes}"
     else
-        
+        puts "Veuillez entrer une heure au format xx:xxAM ou xx:xxPM"
     end
 else 
     puts "Veuillez entrer une heure au format xx:xxAM ou xx:xxPM"
 end
+
